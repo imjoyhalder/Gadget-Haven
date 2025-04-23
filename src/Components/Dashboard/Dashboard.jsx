48cb1c620cc15fc1a7e0998d56fad0002d80a553
imjoyhalder <joyhalder00113355@gamil.com>
2025-04-23 18:34:48 +0600

    Toal cost section added

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -9,7 +9,8 @@ const Dashboard = () => {
     const [cartList, setCartList] = useState([]);
     const [storedCartIds, setStoredCartIds] = useState(getDataFormLocalStorage());
     const [sort, setSort] = useState('');
-    const [activeTab, setActiveTab] = useState('cart'); 
+    const [activeTab, setActiveTab] = useState('cart');
+    const [totalCost, setTotalCost] = useState(0)
 
     useEffect(() => {
         fetch('gadgetsData.json')
@@ -49,29 +50,34 @@ const Dashboard = () => {
         setStoredCartIds(updatedIdList);
     };
 
+    const handleTotalCost = () => {
+        const newTotalCost = cartList.reduce((total, cart) => {
+            return total + parseInt(cart.price);
+        }, 0);
+        setTotalCost(newTotalCost);
+    };
+
     return (
         <div>
             <ToastContainer />
             <div className='text-white text-center pt-6 px-4 md:px-16 lg:px-52 space-y-3 py-10' style={{ background: 'rgb(149, 56, 226)' }}>
                 <h1 className='font-bold text-2xl'>Dashboard</h1>
                 <p className='text-sm text-gray-200'>Explore the latest gadgets that will take your experience to the next level.</p>
-                
+
                 {/* Tab Buttons */}
                 <div className='space-x-3'>
                     <button
                         onClick={() => setActiveTab('cart')}
-                        className={`btn btn-default w-24 md:w-28 rounded-3xl ${
-                            activeTab === 'cart' ? 'bg-white text-purple-700 font-bold' : 'bg-purple-600 text-white'
-                        }`}
+                        className={`btn btn-default w-24 md:w-28 rounded-3xl ${activeTab === 'cart' ? 'bg-white text-purple-700 font-bold' : 'bg-purple-600 text-white'
+                            }`}
                     >
                         Cart
                     </button>
 
                     <button
                         onClick={() => setActiveTab('wishlist')}
-                        className={`btn btn-default w-24 md:w-28 rounded-3xl ${
-                            activeTab === 'wishlist' ? 'bg-white text-purple-700 font-bold' : 'bg-purple-600 text-white'
-                        }`}
+                        className={`btn btn-default w-24 md:w-28 rounded-3xl ${activeTab === 'wishlist' ? 'bg-white text-purple-700 font-bold' : 'bg-purple-600 text-white'
+                            }`}
                     >
                         WishList
                     </button>
@@ -89,6 +95,11 @@ const Dashboard = () => {
                             </button>
                             <button className='btn btn-default border-purple-500 rounded-3xl text-white font-bold bg-purple-600'>Purchase</button>
                         </div>
+                    </div>
+
+                    {/* total cost */}
+                    <div className='text-center text-lg font-semibold'>
+                        <h1>Total cost: <span>${totalCost}</span> </h1>
                     </div>
 
                     <div className='space-y lg:w-10/12 mx-auto'>
